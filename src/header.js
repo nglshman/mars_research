2f3b801ef0b54fcc4ddc3c5783422ef31a2bbdf6
nglshman <mittelfurfahigkeiten@gmail.com>
2022-01-13 18:37:12 +0200

    adding some styles

--- src/header.js
+++ src/header.js
@@ -1,7 +1,11 @@
+import './header.css'
+
 function Header() {
     return (
-        <div>
-            <h1>Mars research</h1>
+        <div className="header">
+            <div className="header_desc">
+                <h1>Mars research</h1>
+            </div>
         </div>
     );
 }
